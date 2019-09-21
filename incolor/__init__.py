a22281078990a1dd11d62067d5f6927ef3a52806
filwie <filip.wiechec@gmail.com>
2019-09-21 21:57:37 +0200

    Add stripping color codes if output is not a TTY.

--- incolor/__init__.py
+++ incolor/__init__.py
@@ -9,6 +9,7 @@ class ColorError(Exception):
 class Color:
     names = ['black', 'red', 'green', 'yellow', 'blue', 'magenta', 'cyan', 'white']
 
+
 for color_number, color_name in enumerate(Color.names):
     setattr(Color, color_name, color_number)
 
@@ -38,10 +39,11 @@ class ColorCode:
         return f'{self.os_prefix}[3{self.number}m'
 
 
-def incolor(color, *args, sep=' '):
+def incolor(color: int, *args, sep=' ', only_tty=True) -> str:
     text = sep.join(map(str, args))
-    if type(color) is int:
-        return f'{ColorCode(color).code}{text}{ColorCode().reset}'
+    if only_tty and not stdout.isatty():
+        return text
+    return f'{ColorCode(color).code}{text}{ColorCode().reset}'
 
 
 def cprint(*args, **kwargs):
--- tests/test_incolor.py
+++ tests/test_incolor.py
@@ -34,14 +34,22 @@ prefix_posix = f'\u001b[3'
 reset_posix = f'\u001b[0m'
 
 
+@mock.patch("sys.stdout.isatty", return_value=True)
 @pytest.mark.parametrize("test_input,expected",
                          [((Color.blue,), f'{prefix_posix}4m{reset_posix}'),
                           ((Color.blue, 123, 'kek', 'fek'), f'{prefix_posix}4m123 kek fek{reset_posix}')])
 @mock.patch("os.name", "posix")
-def test_incolor(test_input, expected):
+def test_incolor(_, test_input, expected):
     assert incolor(*test_input) == expected
 
 
 @mock.patch("os.name", "posix")
-def test_incolor_separator():
+@mock.patch("sys.stdout.isatty", return_value=True)
+def test_incolor_separator(*args):
     assert incolor(4, 'a', 1, 'b', 2, sep='||') == f'{prefix_posix}4ma||1||b||2{reset_posix}'
+
+
+@mock.patch("os.name", "posix")
+@mock.patch("sys.stdout.isatty", return_value=False)
+def test_strippping_color_codes_if_not_tty(*args):
+    assert incolor(Color.blue, 'Hooray', 123) == 'Hooray 123'
